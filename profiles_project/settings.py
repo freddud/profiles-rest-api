d16291799ce314d6ad2ed080ccfa7e6a106ed4ed
fred <freddhl@yahoo.fr>
2024-05-18 18:17:49 +0200

    Added custom user profile model, manager and migrations

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mjcbr6ewen0@je^qka_e+r#*b(tj=u+g33zq9u__ykngzc85@a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
